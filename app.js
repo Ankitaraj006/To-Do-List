c495c686ac1d90c2f9936355462b24d6f3400833
Ankita Raj <ankita.raj006@gmail.com>
2023-05-02 21:04:00 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Studying", "Playing"];
+let workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+//Date List
+app.get("/", function(req, res) {
+
+  let today = new Date();
+  let options = {
+    weekday :'long',
+    day:'numeric',
+    month:'long'
+  };
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {listTitle: day, newListItems: items});
+});
+
+//Common post for both date list and work
+app.post("/", function(req, res){
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+})
+
+//Work listTitle
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle:"Work List", newListItems:workItems});
+});
+
+//about Page
+
+app.get("/about", function(req,res){
+  res.render("about");
+})
+
+
+app.listen(3000, function(req, res) {
+  console.log("Server is on port 3000");
+});
